b6653a03adc55172c0e92fc0e0b435cb856472f4
Philip Omayuku <pomayuku@yahoo.com>
2022-03-14 17:46:55 +0100

    Api response received and parsed as JSON

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.androdocs.httprequest.HttpRequest;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,10 +19,11 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     String CITY;
-    String API="4fca6ca1fe380e4678a8a94ee475cc10";
+    //String API="c4d36921ee2783e189b8feb168a10a50";
+   // String Url ="https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=c4d36921ee2783e189b8feb168a10a50";
     ImageView search;
     EditText etCity;
-    TextView city,country,temp,forecast,time,hum,minTemp,maxTemp,sunRise,sunSet;
+    TextView city,country,Temp,Forecast,time,hum,minTemp,maxTemp,sunRise,sunSet;
 
 
     @Override
@@ -34,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
         search = (ImageView) findViewById(R.id.search);
         city = (TextView) findViewById(R.id.city_name);
         country =(TextView) findViewById(R.id.country_name);
-        temp=(TextView) findViewById(R.id.temp);
-        forecast=(TextView) findViewById(R.id.forecast);
+        Temp=(TextView) findViewById(R.id.temp);
+        Forecast=(TextView) findViewById(R.id.forecast);
         time = (TextView) findViewById(R.id.lastUpdatedTime);
         hum=(TextView) findViewById(R.id.humidity);
         minTemp=(TextView) findViewById(R.id.min_temp);
@@ -43,10 +46,14 @@ public class MainActivity extends AppCompatActivity {
         sunRise=(TextView) findViewById(R.id.sunrise);
         sunSet=(TextView) findViewById(R.id.sunset);
 
+
+
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CITY=etCity.getText().toString();
+             //   CITY=etCity.getText().toString();
+                new weatherTask().execute();
+
 
             }
         });
@@ -62,11 +69,54 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... strings) {
 
-            String response = HttpRequest.excuteGet( "https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid={"4fca6ca1fe380e4678a8a94ee475cc10"});
+            String response=HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=lagos&units=metric&appid=c4d36921ee2783e189b8feb168a10a50"
 );
+
             return response;
         }
-      //  “https://api.openweathermap.org/data/2.5/weather?q=" + CITY + “&units=metric&appid=” + API);
+
+        @Override
+        protected void onPostExecute(String result){
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject main= jsonObject.getJSONObject("main");
+                JSONObject weather= jsonObject.getJSONArray("weather").getJSONObject(0);
+                JSONObject sys = jsonObject.getJSONObject("sys");
+
+                String city_name = jsonObject.getString("name");
+                String country_name = sys.getString("country");
+                String temp = main.getString("temp");
+                String forecast = weather.getString("description");
+                Long lastUpdatedTime= jsonObject.getLong("dt");
+                String lastUpdatedTimeText="Last Updated at:"+new SimpleDateFormat("dd/MM/yyyy hh:mm",Locale.ENGLISH).format(new Date(lastUpdatedTime *1000));
+                String Humidity = main.getString("humidity");
+                String min_temp = main.getString("temp_min");
+                String max_temp = main.getString("temp_max");
+                Long rise = sys.getLong("sunrise");
+                String sunrise = new SimpleDateFormat("hh:mm",Locale.ENGLISH).format(new Date(rise *1000));
+                Long set = sys.getLong("sunset");
+                String sunset = new SimpleDateFormat("hh:mm", Locale.ENGLISH).format(new Date(set *1000));
+
+                city.setText(city_name);
+                country.setText(country_name);
+                Temp.setText(temp +"\u2103");
+                Forecast.setText(forecast);
+                time.setText(lastUpdatedTimeText);
+                hum.setText(Humidity);
+                minTemp.setText(min_temp+"\u2103");
+                maxTemp.setText(max_temp+"\u2103");
+                sunRise.setText(sunrise);
+                sunSet.setText(sunset);
+
+
+
+
+            } catch (JSONException e) {
+               Toast.makeText(MainActivity.this, "Error"+e.toString(),Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
     }
 
 
